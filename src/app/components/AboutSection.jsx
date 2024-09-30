58f1a438aebdb140e0794b92b71130fb995dce53
jobansherkha <jobansherkha@gmail.com>
2024-09-30 12:06:00 -0400

    addee projects

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2 grid grid-cols-2 gap-x-4">
+      <li>Node.js</li>
+      <li>Express</li>
+      <li>PostgreSQL</li>
+      <li>Sequelize</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Next.js</li>
+      <li>TypeScript</li>
+      <li>Tailwind css</li>
+      <li>Redux</li>
+      <li>MongoDb</li>
+      <li>Python</li>
+    </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Computer Programming</li>
+        <li>Lambton College, Mississuaga</li>
+      </ul>
+    ),
+  },
+  // {
+  //   title: "Certifications",
+  //   id: "certifications",
+  //   content: (
+  //     <ul className="list-disc pl-2">
+  //       <li>AWS Cloud Practitioner</li>
+  //       <li>Google Professional Cloud Developer</li>
+  //     </ul>
+  //   ),
+  // },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Frontend Developer with 1.5 years of hands-on experience
+            specializing in React.js, Next.js, and TypeScript. Proven expertise
+            in building and maintaining scalable web applications, with a strong
+            focus on performance optimization, responsive design, and seamless
+            user experiences. Experienced in working on Learning Management
+            Systems (LMS) and proficient in state management (Redux, Context
+            API), API integration, and modern frontend development tools. A
+            proactive team player with a commitment to continuous learning and
+            staying updated with industry trends.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            {/* <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton> */}
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
